eab6cbe8a94fc31e1be205f3366ef77485e781fb
thomasreichmann <thomasalmeidar@gmail.com>
2023-08-02 08:27:14 -0300

    firebase hosting integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
   apiUrl: 'https://api.samsung.thomasar.dev',
+  firebase: {
+    apiKey: 'AIzaSyA9mtnSNMsFDjpRjmAzOu1biiN8cqMfF-w',
+    authDomain: 'samsung-evaluation-ui.firebaseapp.com',
+    projectId: 'samsung-evaluation-ui',
+    storageBucket: 'samsung-evaluation-ui.appspot.com',
+    messagingSenderId: '785556863536',
+    appId: '1:785556863536:web:ce0983e742002c95b78bf5',
+  },
 };
