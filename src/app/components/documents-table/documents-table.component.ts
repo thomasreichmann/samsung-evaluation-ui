b32cc967c5691eda8cc827b950474513f0b9392e
thomasreichmann <thomasalmeidar@gmail.com>
2023-08-01 22:41:54 -0300

    initial table design

--- src/app/components/documents-table/documents-table.component.ts
+++ src/app/components/documents-table/documents-table.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Currency } from '../../models/currency.model';
+import { Document } from '../../models/document.model';
 
 @Component({
   selector: 'app-documents-table',
   templateUrl: './documents-table.component.html',
-  styleUrls: ['./documents-table.component.scss']
 })
 export class DocumentsTableComponent {
+  @Input() documents: Document[] = [];
+  @Input() currencies: Currency[] = [];
 
+  getConvertedValue(document: Document, currencyCode: string): number | null {
+    const conversion = document.convertedCurrencies.find(
+      (cc) => cc.toCurrencyCode === currencyCode
+    );
+    return conversion?.convertedValue || null;
+  }
 }
