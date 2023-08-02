799cc32343c3bbf1d81d915d332c7f9537422032
thomasreichmann <thomasalmeidar@gmail.com>
2023-08-01 22:33:05 -0300

    filter logic

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Currency } from '../../models/currency.model';
+import { CurrencyService } from '../../services/currency.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.scss'],
 })
-export class FiltersComponent {}
+export class FiltersComponent {
+  @Output() refreshData: EventEmitter<any> = new EventEmitter();
+
+  currencies: Currency[] = [];
+  selectedCurrency: string = '';
+  startDate: string = '';
+  endDate: string = '';
+  documentNumber: string = '';
+
+  constructor(private currencyService: CurrencyService) {
+    this.loadCurrencies();
+  }
+
+  loadCurrencies(): void {
+    this.currencyService.getCurrencies().subscribe((data) => {
+      this.currencies = data;
+    });
+  }
+
+  onRefresh(): void {
+    const filters = {
+      documentNumber: this.documentNumber,
+      currencyCode: this.selectedCurrency,
+      startDate: this.startDate,
+      endDate: this.endDate,
+    };
+    this.refreshData.emit(filters);
+  }
+}
